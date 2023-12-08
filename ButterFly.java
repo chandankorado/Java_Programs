222598f9c91521337f50280088ebeacdea8a39ed
Chandan Korado <104411581+chandankorado@users.noreply.github.com>
2023-12-08 23:35:17 +0530

    Add files via upload
    Patterns in java

---
+++ ButterFly.java
@@ -0,0 +1,32 @@
+class ButterFly{
+	public static void main(String args[]){
+		int n = 4;
+		
+		for(int i = 1; i <= n; i++){
+			for(int j = 1; j <= i; j++)
+				System.out.print("* ");
+			
+			for(int j = 1; j <= 2*(n-i); j++){
+				System.out.print("  ");
+				
+			}
+			for(int j = 1; j <= i; j++)
+				System.out.print("* ");
+				
+			System.out.println();
+		}
+		for(int i = 1; i <= n; i++){
+			
+			for(int j = 0; j <= n-i; j++)
+				System.out.print("* ");
+				
+			for(int j = 0; j < 2*(i-1); j++)
+				System.out.print("  ");
+			
+			for(int j = 0; j <= n-i; j++)
+				System.out.print("* ");
+			
+			System.out.println();
+		}
+	}
+}
---
+++ Rectangle.java
@@ -0,0 +1,10 @@
+class Rectangle{
+	public static void main(String args[]){
+		for(int  = 0; i < 5; i++)
+		{
+			for(int j = 0; j < 4; j++)
+				System.out.print("* ");
+			System.out.println();
+		}
+	}
+}
